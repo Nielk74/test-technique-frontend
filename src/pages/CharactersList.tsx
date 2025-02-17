b481c2f85914e53f2d614f3d8dc9698fa049bee8
Antoine Klein <67603819+Nielk74@users.noreply.github.com>
2025-02-17 16:23:56 +0000

    fixed pagination

--- src/pages/CharactersList.tsx
+++ src/pages/CharactersList.tsx
@@ -1,14 +1,26 @@
-import { Link } from "react-router";
-import { DataGrid, GridColDef, GridFilterModel, getGridStringOperators} from '@mui/x-data-grid';
-import { useState } from 'react';
+import { Link, useNavigate } from "react-router";
+import { DataGrid, GridColDef, GridFilterModel, getGridStringOperators } from '@mui/x-data-grid';
+import { useMemo, useRef, useState } from 'react';
 import { useCharacters } from '../hooks/useCharacter';
+import { styles } from "../components/navigation/styled";
 const PAGE_SIZE = 20;
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 const CharactersList = () => {
-    const [page, setPage] = useState(1);
+    const [paginationModel, setPaginationModel] = useState({
+        page: 0,
+        pageSize: PAGE_SIZE,
+    });
     const [filterModel, setFilterModel] = useState<GridFilterModel>({
         items: []
     });
+
     const filters = {
         name: filterModel.items.find(item => item.field === 'name')?.value as string,
         status: filterModel.items.find(item => item.field === 'status')?.value as string,
@@ -17,8 +29,8 @@ const CharactersList = () => {
     };
 
     const filterOperatorContains = getGridStringOperators().find(operator => operator.value === 'contains');
-    const filterOperators =  filterOperatorContains ? [filterOperatorContains] : [];
-    const { characters, info, loading } = useCharacters(page, filters);
+    const filterOperators = filterOperatorContains ? [filterOperatorContains] : [];
+    const { characters, info, loading } = useCharacters(paginationModel.page + 1, filters);
     const columns: GridColDef[] = [
         {
             field: 'image',
@@ -29,38 +41,54 @@ const CharactersList = () => {
             filterable: false,
             sortable: false,
         },
-        { field: 'name', headerName: 'Name', filterable: true, filterOperators },
-        { field: 'status', headerName: 'Status', filterable: true, filterOperators},
+        { field: 'name', headerName: 'Name', filterable: true, filterOperators, minWidth: 200 },
+        { field: 'status', headerName: 'Status', filterable: true, filterOperators },
         { field: 'species', headerName: 'Species', filterable: true, filterOperators },
         { field: 'gender', headerName: 'Gender', filterable: true, filterOperators },
-
     ];
+
+    const rowCountRef = useRef(info?.count || 0);
+
+    const rowCount = useMemo(() => {
+        if (info?.count !== undefined) {
+            rowCountRef.current = info.count;
+        }
+        return rowCountRef.current;
+    }, [info?.count]);
+
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <h1>Characters</h1>
-            <div style={{ height: "70vh", width: '100vw'}}>
-            <DataGrid
-                rows={characters}
-                columns={columns}
-                pagination
-                paginationMode="server"
-                filterMode="server"
-                rowCount={info?.count || 0}
-                loading={loading}
-                pageSizeOptions={[PAGE_SIZE]}
-                paginationModel={{
-                    page: page - 1,
-                    pageSize: PAGE_SIZE,
-                }}
-                onPaginationModelChange={(model) => setPage(model.page + 1)}
-                filterModel={filterModel}
-                onFilterModelChange={setFilterModel}
-                sx={{ backgroundColor: 'white' }}
-            />
+        <ThemeProvider theme={darkTheme}>
+            <div style={{ margin: '20px' }}>
+                <Link to="/" style={styles.link}>Home</Link>
+                <h1>Characters</h1>
+                <div style={{ height: "70vh", maxWidth: "90vw" }}>
+                    <DataGrid
+                        rows={characters}
+                        columns={columns}
+                        pagination
+                        paginationMode="server"
+                        filterMode="server"
+                        rowCount={rowCount}
+                        loading={loading}
+                        pageSizeOptions={[PAGE_SIZE]}
+                        paginationModel={paginationModel}
+                        onPaginationModelChange={setPaginationModel}
+                        filterModel={filterModel}
+                        onFilterModelChange={setFilterModel}
+                        sx={{
+                            '.MuiDataGrid-row:hover': {
+                                cursor: 'pointer',
+                            },
+                        }}
+                        onRowClick={(row) => navigate(`/characters/${row.id}`)}
+                        disableRowSelectionOnClick={true}
+                    />
+                </div>
             </div>
-        </div>
+        </ThemeProvider>
     );
-    };
+};
 
 export default CharactersList;
