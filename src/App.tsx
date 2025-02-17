17fef8f31c521da22e604c0a9b5d74b1891d377e
Antoine Klein <67603819+Nielk74@users.noreply.github.com>
2025-02-17 16:27:37 +0000

    enhance style

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { useState } from 'react';
 
 import Carousel from './components/carousel/Carousel';
 import { Link } from 'react-router';
-
+import { styles } from './components/navigation/styled';
 
 function App() {
   const [, setCharacterId] = useState(1);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <h2>Characters</h2>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 24}}>
+      <img src="./portal.png" alt="Rick and Morty" style={{ height: '30vh', paddingTop: '50px'}} />
       <Carousel setCharacterId={setCharacterId} />
-      <Link to="/characters/list">{`View more >`}</Link>
+      <Link to="/characters/list" style={styles.link}>{`View more`}</Link>
     </div>
   )
 }
---
+++ src/components/navigation/styled.tsx
@@ -0,0 +1,13 @@
+export const styles = {
+    link: {
+        display: 'inline-block',
+        backgroundColor: 'rgb(17 138 68)',
+        padding: '8px 16px',
+        margin: '8px',
+        textDecoration: 'none',
+        color: 'rgb(245, 253, 244)',
+        // border: '1px solid rgb(2 49 70)',
+        borderRadius: '25px',
+        boxShadow: 'rgba(147, 255, 58, 0.6) 0px 0px 20px'
+    }
+} as const;
